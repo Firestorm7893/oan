f0b2e21557e633cd38ff9568fcc52fd93991c798
Firestorm7893 <luckyluca2014@gmail.com>
2020-04-30 21:46:03 +0200

    eeeeeeee

--- js/menu.js
+++ js/menu.js
@@ -5,12 +5,12 @@ var i = 0;
 
 for(i = 0; i < btns.length; i++){	
 	if("#" + btns[i].id == window.location.hash){
-	
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
 		$("#mydiv").load("./pages/" + btns[i].id +".html");
 		btns[i].className += " active";
 		break;
 	} else if(i == btns.length-1){
-		$("#home").className += " active";
 		$("#mydiv").load("./pages/home.html");
 	}
 }
