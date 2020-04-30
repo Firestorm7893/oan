7a8f9da14e63abf1259c7805871ffe50da0d98b9
Firestorm7893 <luckyluca2014@gmail.com>
2020-04-30 20:53:29 +0200

    perchè non va wtf?

--- js/menu.js
+++ js/menu.js
@@ -6,15 +6,22 @@ var i = 0;
 alert(window.location.hash);
 
 for(i = 0; i < btns.length; i++){
-	if("#" + btns[i].id==window.location.hash){
+	
+	alert(btns[i].id + " " + window.location.hash);
+
+	if("#" + btns[i].id == window.location.hash){
+	
 		$("#mydiv").load("./pages/" + btns[i].id +".html");
 	} else if(i == btns.length-1){
+		
 		$("#mydiv").load("./pages/home.html");
 	}
 }
 
 for (i = 0; i < btns.length; i++) {
+	
   btns[i].addEventListener("click", function() {
+  
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
   this.className += " active";
