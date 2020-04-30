2e5b2016024b59336d5cf54763187109ac184478
Firestorm7893 <luckyluca2014@gmail.com>
2020-04-30 18:20:10 +0200

    fix javascript eyes

--- js/eyes.js
+++ js/eyes.js
@@ -1,7 +1,35 @@
+const ScrollMagic = window.ScrollMagic;
+const TweenMax = window.TweenMax;
+const TweenLite = window.TweenLite;
 
-var doc;
-function ready(document){
-	
-	doc = document;
+
+var controller = new ScrollMagic.Controller();
+var tween = TweenMax.to("#logotarget", 1, {backgroundColor: "none", width: "-=300",y: "+=173"});
+
+var scene = new ScrollMagic.Scene({offset: 0, duration: 200})
+										.setTween(tween)
+										.addTo(controller);
+
+var $circle = $('.pupil'), $area = $('#eye');
+
+function moveCircle(e) {
+	TweenLite.to($circle, 0.3, {
+	  css: {
+		left: e.pageX-15,
+		top: e.pageY -200
+	  }
+	});
 }
+
+
+var flag = false;
+$($area).mouseenter(function(){
+	flag = true;
+	TweenLite.to($circle,0.4,{autoAlpha:1})
+	$($area).on('mousemove', moveCircle);
+});
+$($area).mouseout(function(){
+	flag = false;
+	TweenLite.to($circle,0.4,{autoAlpha:0})
+});
 
