43cbe7f8a3e19d23fef4d22f9ea20c5046d199d7
Firestorm7893 <luckyluca2014@gmail.com>
2020-04-30 20:41:13 +0200

    ricorda posto

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,22 @@
-$("#mydiv").load("./pages/home.html")
-
-const queryString = window.location.search;
-alert(queryString);
-
-
 
 var btns = document.getElementsByClassName("menubtn");
-for (var i = 0; i < btns.length; i++) {
+
+var i = 0;
+
+for(i = 0; i < btns.length; i++){
+	if("#" + btns[i].id==window.location.hash){
+		$("#mydiv").load("./pages/" + btns[i].id +".html");
+	} else if(i == btns.length-1){
+		$("#mydiv").load("./pages/home.html");
+	}
+}
+
+for (i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
   this.className += " active";
 	$("#mydiv").load("./pages/" + this.id +".html");
-	  window.location.hash = "#" + this.id;
+	window.location.hash = "#" + this.id;
   });
 }
