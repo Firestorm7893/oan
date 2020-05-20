2a2660d63f56d074b86d4b246554c35c980457c0
Firestorm7893 <luckyluca2014@gmail.com>
2020-05-20 18:58:00 +0200

    qwww

--- js/menu.js
+++ js/menu.js
@@ -6,11 +6,11 @@ for (i = 0; i < btns.length; i++) {
     if ("#" + btns[i].id == window.location.hash) {
         var current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
-        $("#mydiv").load("./pages/" + btns[i].id + ".html");
+        $("#mydiv").load("./" + btns[i].id + ".html");
         btns[i].className += " active";
         break;
     } else if (i == btns.length - 1) {
-        $("#mydiv").load("./pages/home.html");
+        $("#mydiv").load("./home.html");
     }
 }
 
@@ -21,7 +21,7 @@ for (i = 0; i < btns.length; i++) {
         var current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
-        $("#mydiv").load("./pages/" + this.id + ".html");
+        $("#mydiv").load("./" + this.id + ".html");
         window.location.hash = "#" + this.id;
     });
 }
